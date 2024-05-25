60163b95e2e81b17846a98a17c705e7c6490e65a
DaniloSam <danilobrown17@gmail.com>
2024-05-25 18:23:07 -0300

    Conclusão do desafio DIO

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO:Conhecer a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o usuário
+        // Obter pela scanneer os valores digitados
+        System.out.println("Digite o seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o sua Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu numero");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o seu saldo");
+        double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque");
     }
 }
